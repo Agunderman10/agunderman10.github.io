2843364213441cd2b013baac2976ea6d4d8c080c
Andrew Gunderman <agunderman10@gmail.com>
2018-12-24 21:39:02 -0500

    Refactor code for readability

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
 var myVar;
 
-function myFunction() {
+function loader() {
   myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("loaderPageContent").style.display = "block";
 }
