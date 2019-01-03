968fb65d1c2ff4bec0db6b4a85ff7efec70b0590
Andrew Gunderman <agunderman10@gmail.com>
2019-01-03 18:41:25 -0500

    Added ageAutoUpdateTest

--- ageAutoUpdate.js
+++ ageAutoUpdate.js
@@ -1,2 +1,32 @@
-var age = "18";
+//the ninth month is october because javascript months go from 0-11
+var dateToday = new Date();
+var myBirthday = new Date(2000,9,25);
+var age = 0;
+
+//if the date today is before october 25 then age starts at -1
+//so we can add the difference of today's year and 2000 to it after to find age
+/*
+if(dateToday.getMonth() <= 9)
+{
+    if(dateToday.getDate() >= 25) 
+    {
+        ++age;
+    }
+    --age;
+}
+*/
+
+//test to see if auto update works. will remove
+if(dateToday.getMonth() <= 0)
+{
+    if(dateToday.getDate() >= 4) 
+    {
+        ++age;
+    }
+    --age;
+}
+
+age = (dateToday.getFullYear() - myBirthday.getFullYear()) + age;
 document.getElementById("ageAutoUpdate").innerHTML = age;
+                                                        
+
