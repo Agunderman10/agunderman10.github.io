beb0f66998892eb3d726c40e4de662cf8c59f1b1
Andrew Gunderman <agunderman10@gmail.com>
2019-01-04 00:07:24 -0500

    Changed ageAutoUpdate to actual birthday after successful test

--- ageAutoUpdate.js
+++ ageAutoUpdate.js
@@ -5,21 +5,9 @@ var age = 0;
 
 //if the date today is before october 25 then age starts at -1
 //so we can add the difference of today's year and 2000 to it after to find age
-/*
 if(dateToday.getMonth() <= 9)
 {
     if(dateToday.getDate() >= 25) 
-    {
-        ++age;
-    }
-    --age;
-}
-*/
-
-//test to see if auto update works. will remove
-if(dateToday.getMonth() <= 0)
-{
-    if(dateToday.getDate() >= 4) 
     {
         ++age;
     }
